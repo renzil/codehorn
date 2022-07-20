e247220aa7090f005432d55845507445c90392b7
Renzil DSouza <renzil@gmail.com>
2022-07-20 11:17:51 +0530

    build: ignoring all generated assets

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
+fs.mkdirSync('public');
+
 const testFolder = 'src/partials/';
 const files = fs.readdirSync(testFolder).map(file => testFolder + file);
 const partials = files.reduce((prev, current, index) => prev + `${index === 0 ? '' : ' '}-p ` + current, "");
@@ -15,5 +17,6 @@ const callback = (err, stdout, stderr) => {
     console.log(stdout);
 }
 exec(`node_modules/.bin/mustache ${partials} src/data.json src/index.mustache public/index.html `, callback);
+exec(`node_modules/.bin/mustache src/data.json src/404.mustache public/404.html `, callback);
 exec(`node_modules/.bin/tailwind -i src/main.css -o public/bundle.css`, callback);
 exec(`node_modules/.bin/rollup -c`, callback);
