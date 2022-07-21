2998deef75765d504a2aa961d542710c92f98b9e
Renzil DSouza <renzil@gmail.com>
2022-07-21 15:37:24 +0530

    cleanup: remove rollup, dead code, comments

--- scripts/build.js
+++ scripts/build.js
@@ -21,4 +21,3 @@ const callback = (err, stdout, stderr) => {
 exec(`node_modules/.bin/mustache ${partials} src/data.json src/index.mustache public/index.html `, callback);
 exec(`node_modules/.bin/mustache src/data.json src/404.mustache public/404.html `, callback);
 exec(`node_modules/.bin/tailwind -i src/main.css -o public/bundle.css`, callback);
-exec(`node_modules/.bin/rollup -c`, callback);
